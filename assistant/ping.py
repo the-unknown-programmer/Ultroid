f85e56a29ddbf1c6cd94ec47e26cb776f184b33d
Black Minded <67266951+the-unknown-programmer@users.noreply.github.com>
2021-03-01 17:24:06 +0530

    Update ping.py

--- assistant/ping.py
+++ assistant/ping.py
@@ -16,5 +16,7 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**Pong!!**\n `{ms}ms`",
+        f"**█▀█ █ █▄░█ █▀▀ █ \n
+
+█▀▀ █ █░▀█ █▄█ ▄**\n `{ms}ms`",
     )
